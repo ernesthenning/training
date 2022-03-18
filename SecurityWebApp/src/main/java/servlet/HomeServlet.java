dbe910f2c69f7eeb05d245cb0d7ce90db1731507
Vladislav Malinnikov <vladmalff@gmail.com>
2022-03-18 17:18:50 +0300

    Added HomeServlet

--- SecurityWebApp/src/main/java/servlet/HomeServlet.java
+++ SecurityWebApp/src/main/java/servlet/HomeServlet.java
@@ -1,5 +1,30 @@
 package servlet;
 
-public class HomeServlet {
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet ({"/", "/index"})
+public class HomeServlet extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public HomeServlet() {
+        super();
+    }
+    
+    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/views/homeView.jsp");
+        dispatcher.forward(request, response);
+    }
+    
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        doGet(request, response);
+    }
 
 }
